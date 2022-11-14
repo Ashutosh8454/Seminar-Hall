e5e1d5fca1c4caeda7f32a19d893377b2d6169a6
sudharm2004 <sudharmjadhav2004@gmail.com>
2022-11-14 12:36:06 +0530

    validation for end time less than start time done

--- book_now.php
+++ book_now.php
@@ -69,6 +69,31 @@
         }
         let today=year+"-"+month+"-"+day;
         document.getElementById("i6").setAttribute("min",today);
+
+        let starttime=document.getElementById("i4");
+        let endtime=document.getElementById("i5");
+        console.log(starttime.value);
+        // starttime.addevent
+        endtime.addEventListener('click',()=>{
+            if(starttime.value.length==0)
+            {
+                alert("Please select start time first");
+                endtime.value="";
+            }else{
+                endtime.value=starttime.value;
+            }
+        })
+        endtime.addEventListener("change",function(){
+            if(starttime.value.length==0)
+            {
+                alert("Please select start time first");
+                endtime.value="";
+            }
+            if (endtime.value<starttime.value) {
+                alert("End time should be greater than start time");
+                endtime.value="";
+            }
+        });
     </script>
 </body>
 
