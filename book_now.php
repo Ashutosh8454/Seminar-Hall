a2a1f35e90205e9f03fdfde8437d3ea74baeb29c
Ashutosh8454 <ashutosh703325@gmail.com>
2022-11-15 23:49:08 +0530

    booking is sucess signup data store completed

--- book_now.php
+++ book_now.php
@@ -1,3 +1,79 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION['teacher_id']) ||  $_SESSION['teacher_id'] != true)
+    {
+        header("location: Login.php");
+        exit;
+    }
+
+    include 'connection.php';
+
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+        
+        if(isset($_POST["i1"])){
+            $event_name = $_POST["i1"];
+        }
+        if(isset($_POST["i2"])){
+            $description = $_POST["i2"];
+        }
+        
+        
+        if(isset($_POST["i4"])){
+            $temp_start = $_POST["i4"];
+        }
+        if(isset($_POST["i5"])){
+            $temp_end = $_POST["i5"];
+        }
+        if(isset($_POST["i6"])){
+            $pick_date = $_POST["i6"];
+        }
+        $teacher_id = $_SESSION["teacher_id"];
+        $organizer = $_SESSION["teacher_name"];
+        
+        
+        $sql="SELECT * FROM `main_table` 
+              WHERE pick_date = '$pick_date' AND teacher_id = $teacher_id";
+        $result = mysqli_query($con,$sql);
+        
+        $flag = 0;
+        while($row = mysqli_fetch_assoc($result))
+        {
+            $start_time = $row["start_time"];
+            $end_time = $row["end_time"];
+            if(($temp_start > $start_time && $temp_start < $end_time) || 
+                ($temp_end > $start_time && $temp_end < $end_time))
+            {
+                $flag = 1;
+                break;
+            }
+        }
+        if($flag == 1)
+        {
+            echo "<script> alert(' This time slot has been booked already! ') </script>";
+        }
+        else{
+            $sql = "INSERT INTO main_table VALUES ('$event_name','$description','$organizer','$temp_start','$temp_end','$pick_date','$teacher_id')";
+            $result = mysqli_query($con,$sql);
+            if($result){
+                echo "<script> alert('Record successfully inserted into the database!')</script>";
+            }
+            else{
+                echo mysqli_error($con);
+            }
+        }
+
+        
+    }
+
+    //Logout
+    if(isset($_POST['logout'])){
+        header("location: logout.php");
+    }   
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,40 +83,86 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Booking | Page</title>
     <link rel="stylesheet" href="Css/style.css">
- 
 </head>
 
 <body>
     <h1 class="header">Booking Form :</h1>
     <hr class="hr">
-    <div class="box1">
-        <div class="name">
-            <font id="ename">Event Name :</font>
-            <input type="text" id="i1" required>
-            <font id="desc">Description :</font>
-            <input type="text" id="i2" required>
-            <font id="org">Orginizer  :</font>
-            <input type="text" id="i3" required>
-            <font id="st">Start time  :</font>
-            <input type="time" id="i4" required>
-            <font id="et">End time   :</font>
-            <input type="time" id="i5" required>
-            <font id="pd">Pick date   :</font>
-            <input type="date" id="i6" required>
+    <form action="logout.php" method="post">
+        <nav>
+            <a href="signup.php" target="_blank">
+                <input type="submit" value="Logout" class="logout" name="logout">
+            </a>
+        </nav>
+    </form>
+    <form action="#" method="post">
+        <div class="box1">
+            <div class="name">
+                <font id="ename">Event Name :</font>
+                <input type="text" id="i1" name="i1" required>
+                <font id="desc">Description :</font>
+                <input type="text" id="i2" name="i2" required>
+                <font id="org">Orginizer  :</font>
+                <input type="text" id="i3" name="i3" value="<?php echo $_SESSION["teacher_name"]  ?>" readonly>
+                <font id="st">Start time  :</font>
+                <input type="time" id="i4" name="i4" required>
+                <font id="et">End time   :</font>
+                <input type="time" id="i5" name="i5" required>
+                <font id="pd">Pick date   :</font>
+                <input type="date" id="i6" name="i6" required>
+            </div>
+            <div class="tc">
+                <font id="tnc">By clicking on this, I agree <a href="tnc" target="_blank" id="tac">"terms and conditions"</a></font>
+                <input type="checkbox" id="cb" required>
+                <input type="submit" value="Submit" id="submit">
+            </div>
         </div>
-        <div class="tc">
-            <font id="tnc">By clicking on this, I agree <a href="tnc" target="_blank" id="tac">"terms and conditions"</a></font>
-            <input type="checkbox" id="cb" required>
-            <input type="button" value="Submit" id="submit">
-        </div>
-    </div>
+    </form>
     <div class="box2">
         <div class="box3">
             <marquee behavior="scroll" direction="right" class="ue">UPCOMING EVENTS</marquee>
         </div>
     </div>
     <div class="box4">
-        <ul>
+        
+      
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Event Name</th>
+                        <th>Start Time</th>
+                        <th>End Time</th>
+                        <th>Organizer</th>
+                        <th>Phone Number</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                    <?php
+                        $temp_date = date("Y/m/d");
+                        $date = str_replace("/","-",$temp_date);
+                        // echo $date;
+                        $sql="SELECT main_table.event_name,main_table.organizer,main_table.start_time,main_table.end_time,teacher_table.phone_no 
+                              from main_table,teacher_table
+                              where pick_date = '$date' Order by start_time ASC";
+                        $result = mysqli_query($con,$sql);
+                        
+                        while($row = mysqli_fetch_assoc($result))
+                        {
+                            echo '<tr>
+                                    <td class="el">'.$row["event_name"].'</td>
+                                    <td class="el">'.$row["start_time"].'</td>
+                                    <td class="el">'.$row["end_time"].'</td>
+                                    <td class="el">'.$row["organizer"].'</td>
+                                    <td class="el">'.$row["phone_no"].'</td>
+
+                                  </tr>';
+                        }
+                    ?>
+                    
+                </tbody>
+            <table>
+            <!-- <li class="el">EVENT NAME (DATE)</li>
             <li class="el">EVENT NAME (DATE)</li>
             <li class="el">EVENT NAME (DATE)</li>
             <li class="el">EVENT NAME (DATE)</li>
@@ -49,9 +171,8 @@
             <li class="el">EVENT NAME (DATE)</li>
             <li class="el">EVENT NAME (DATE)</li>
             <li class="el">EVENT NAME (DATE)</li>
-            <li class="el">EVENT NAME (DATE)</li>
-            <li class="el">EVENT NAME (DATE)</li>
-        </ul>
+            <li class="el">EVENT NAME (DATE)</li> -->
+        
     </div>
 </body>
 <script src="js/booknowval.js"></script>
--- signup.php
+++ signup.php
@@ -1,4 +1,47 @@
-<!Doctype html>
+<?php
+session_start();
+
+include 'connection.php';
+
+if(isset($_POST['submit']))
+{
+  $id=$_POST['id'];
+
+  $fname= $_POST['fname'];
+  $femail= $_POST['femail'];
+  $fphone= $_POST['fphone'];
+  $fpass= $_POST['fpass'];
+  $fcpass=$_POST['fcpass'];
+  if ($_SERVER["REQUEST_METHOD"] == "POST") 
+   {  
+
+//username must be unique
+      $act="Active";
+     $stmt="SELECT teacher_id FROM teacher_table WHERE teacher_id='$id'";
+     $result=mysqli_query($con,$stmt);
+     if(mysqli_num_rows($result)>0)
+
+    
+     {
+        echo "Opps Id exists already. Try Another !! <br>";
+          
+     }
+     elseif($id!="" && $fname!="" && $femail!="" && $fphone!="" && $fpass!="" && $fcpass!="")
+          {
+            $query= "insert into teacher_table(teacher_id,teacher_name,status,password,email,phone_no) VALUES ('$id','$fname','$act','$fpass','$femail','$fphone')";
+            $data=mysqli_query($con, $query);
+             if($data)
+             {
+              header("location: login.php");
+             }
+            }
+            else
+            {
+             $main_err="Something is missing .All things are necessary";
+            }
+          }
+      }
+      ?>
 <html>
 
 <head>
@@ -16,7 +59,7 @@
 
         <div class="page">
             <div class="title">--Sign up--</div>
-            <form action="regis.php" name="registration" onsubmit="return validateForm()" method="post">
+            <form action="" name="registration" onsubmit="return validateForm()" method="POST">
 
                 <div class="formdesign" id="name">
                     <input type="text" name="fname" placeholder="Enter full name*" required>
@@ -42,7 +85,7 @@
                 <b><span class="formerror"></span></b>
                 </div>
 
-                <button type="submit" value="submit">Sign up</button>
+                <button type="submit" name="submit" value="submit">Sign up</button>
 
             </form>
             <!-- <div class="fid">
@@ -51,7 +94,7 @@
             <div class="already">
                 <p>Already have an account?<a href="login.php">login</a></p>
             </div>
-
+            
 </body>
 
 </html>
