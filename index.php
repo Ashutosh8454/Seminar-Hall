13413d9e5d297a0c921739c407c8a68f1d980cb3
Ashutosh8454 <ashutosh703325@gmail.com>
2022-11-15 22:29:10 +0530

    making home page responsive

--- index.php
+++ index.php
@@ -6,34 +6,51 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home | Page</title>
-    <link rel="stylesheet" href="Css/style.css">
+    <link rel="stylesheet" href="home_page.css">
 </head>
 
 <body>
-    <div class="home_page">
-        <a href="https://mitacsc.ac.in/" target="_blank">
-            <img src="img/logo.png" class="logo">
-        </a>
-        <nav>
-            <a href="login.php" target="_blank">
-                <button class="login">Log In</button>
+    <div id="navbar">
+        <header id="header">
+            <a href="https://mitacsc.ac.in/index.php" target="_blank" rel="noopener noreferrer" class="imgbox">
+                <img src="img/logo.png" alt="404 - not found">
             </a>
-            <hr class="vline">
-            <a href="signup.php" target="_blank">
-                <button class="signup">Sign_up</button>
-            </a>
-        </nav>
-        <hr class="hline">
-        <img src="seminar hall.jpg" class="seminarhall">
+            <nav>
+                <ul id="list">
+                    <li>
+                        <a href="login.php">
+                            <button id="login_btn">LOGIN</button>
+                        </a>
+                    </li>
+                    <hr>
+                    <li>
+                        <a href="signup.php">
+                            <button id="signup_btn">SIGN UP</button>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+    <hr class="hline">
+    <div>
+        <img src="img/seminarhall.jpg" alt="404  not found" class="seminarhall">
+    </div>
+    <div class="largcont">
         <div class="container">
-            <font class="f1">TO BOOK THE SEMINAR HALL</font>
-            <font class="f2">click the button below</font>
-            <a href="book_now.php" target="_blank">
-                <button class="book_now">Book now</button>
-            </a>
-            <a href="learn_more.html" target="_blank">
-                <button class="learn_more">Learn more</button>
-            </a>
+            <div class="fonts">
+                <font class="f1">TO BOOK THE SEMINAR HALL</font>
+                <font class="f2">click the button below</font>
+            </div>
+            <div class="btns">
+                <a href="login.php" target="_blank">
+                    <button class="book_now">Book now</button>
+                </a>
+                <div style="width: 20px"></div>
+                <a href="learn_more.html" target="_blank">
+                    <button class="learn_more">Learn more</button>
+                </a>
+            </div>
         </div>
     </div>
     <div class="container2">
@@ -43,30 +60,35 @@
         <hr class="underline">
     </div>
     <div class="lastcontainer">
-        <div class="c1">
-            <img src="#" alt="no image found !">
+        <div class="c">
+            <img src="#" alt="1">
         </div>
-        <div class="c2">
-            <img src="#" alt="no image found !">
+        <div class="c">
+            <img src="#" alt="2">
         </div>
-        <div class="c3">
-            <img src="#" alt="no image found !">
+        <div class="c">
+            <img src="#" alt="3">
         </div>
-        <div class="c4">
-            <img src="#" alt="no image found !">
+        <div class="c">
+            <img src="#" alt="4">
         </div>
-    </div>  
+    </div>
     <footer class="footer">
-        <font class="footer_f">© 2022 WWW.MITACSC.AC.IN - ALL RIGHTS RESERVED</font>
-        <a href="https://www.linkedin.com/company/mit-acsc-alandi-pune./?originalSubdomain=in" target="_blank">
-            <img src="linkdin.png" class="linkdin">
-        </a>
-        <a href="https://www.facebook.com/MITACSCALANDI/" target="_blank">
-            <img src="facebook.png" class="facebook">
-        </a>
-        <a href="https://g.page/MITACSC?share" target="_blank">
-            <img src="location.png" class="location">
-        </a>
+        <div id="div1">
+            <font class="footer_f">© 2022 WWW.MITACSC.AC.IN - ALL RIGHTS RESERVED</font>
+        </div>
+        <div id="div2">
+            <a href="https://www.linkedin.com/company/mit-acsc-alandi-pune./?originalSubdomain=in" target="_blank">
+                <img src="linkdin.png" class="img" id="img1">
+            </a>
+            <a href="https://www.facebook.com/MITACSCALANDI/" target="_blank">
+                <img src="facebook.png" class="img" id="img2">
+            </a>
+            <a href="https://g.page/MITACSC?share" target="_blank">
+                <img src="location.png" class="img" id="img3">
+            </a>
+        </div>
     </footer>
 </body>
+
 </html>
